61af4d85f0575384ae608bd498ba7d99e4791c74
Divya Singh <ps3312806@gmail.com>
2026-09-15 22:31:34 +0530

    Time: 1 ms (100.00%) | Memory: 42.9 MB (79.40%) - LeetSync

---
+++ 2559-maximum-number-of-non-overlapping-palindrome-substrings/maximum-number-of-non-overlapping-palindrome-substrings.java
@@ -0,0 +1,42 @@
+class Solution {
+    public int maxPalindromes(String s, int k) {
+        int n = s.length();
+        int count = 0;
+        int i = 0;
+
+        while (i + k <= n) {
+
+            // Check palindrome of length k
+            if (isPalindrome(s, i, i + k - 1)) {
+                count++;
+                i += k;
+            }
+
+            // Check palindrome of length k + 1
+            else if (i + k + 1 <= n &&
+                     isPalindrome(s, i, i + k)) {
+                count++;
+                i += k + 1;
+            }
+
+            else {
+                i++;
+            }
+        }
+
+        return count;
+    }
+
+    private boolean isPalindrome(String s, int left, int right) {
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) {
+                return false;
+            }
+
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+}
